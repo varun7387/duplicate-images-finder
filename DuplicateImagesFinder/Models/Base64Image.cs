6ecde571b0b62ee4f12a9e9defca904e9a045d4c
Varun Ghaswala <varun7387@gmail.com>
2020-09-28 11:23:48 +1300

    simple model for storing images in memory

---
+++ DuplicateImagesFinder/Models/Base64Image.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuplicateImagesFinder.Models
+{
+    class Base64Image
+    {
+        public string Path { get; private set; }
+
+        public int Width { get; private set; }
+
+        public int Height { get; private set; }
+
+        public string Base64 { get; private set; }
+
+        public Base64Image(
+            string path,
+            int width,
+            int height,
+            string base64
+        )
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+            if (string.IsNullOrEmpty(base64))
+            {
+                throw new ArgumentNullException(nameof(base64));
+            }
+
+            if (width <= 0 || height <= 0)
+            {
+                throw new ArgumentNullException($"Width and height must be greater than 0.");
+            }
+
+            Path = path;
+            Width = width;
+            Height = height;
+            Base64 = base64;
+        }
+    }
+}
