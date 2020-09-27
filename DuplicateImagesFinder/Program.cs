e7d34311cd43791bffc06112cd2d04c2c7cca2ab
Varun Ghaswala <varun7387@gmail.com>
2020-09-28 11:06:21 +1300

    setup projects and solution

---
+++ DuplicateImagesFinder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DuplicateImagesFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
