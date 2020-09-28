0571dc5e563417b6ece78a6bc171f916256823d8
Varun Ghaswala <varun7387@gmail.com>
2020-09-28 13:14:28 +1300

    updated program to be simple and streamlined

--- DuplicateImagesFinder/Program.cs
+++ DuplicateImagesFinder/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DuplicateImagesFinder.Services;
+using System;
 
 namespace DuplicateImagesFinder
 {
@@ -6,7 +7,18 @@ namespace DuplicateImagesFinder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var duplicateImageFinder = new DuplicateImageFinder();
+
+            if (args == null || args.Length == 0)
+            {
+                throw new ArgumentNullException($"You must specify the path for the directory where to run the application.");
+            }
+
+            var path = args[0];
+            duplicateImageFinder.Process(path);
+
+            Console.WriteLine("Press any key to stop...");
+            Console.ReadKey();
         }
     }
 }
